9cd554f2bccb07f87256ebc4c245ba61b48fd316
qqq <58675927+qqq1357962@users.noreply.github.com>
2021-02-19 16:48:28 +0800

    Update file_operations.py

--- file_operations.py
+++ file_operations.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import numpy as np
 import os
-
+import matplotlib.pyplot as plt
 
 def get_pic_paths_in_folder(file_dir):
     L = []
@@ -15,20 +15,50 @@ def load_np_image(file_path):
      Returns:numpy array ranging from 0 to 255
      dtype=unit8
   '''
-  im = np.array(Image.open(file_path))
+  im = np.array(Image.open(file_path),dtype='float32')
   im=np.swapaxes(im,1,2)
   im=np.swapaxes(im,0,1)
   im=np.reshape(im,(1,im.shape[0],im.shape[1],im.shape[2]))
   return im
-'''
-def resize():
-  return 0
-im=load_np_image('eiffel_tower.jpg')
-print(type(im))
 
-print(im.dtype)
+def Tensor_to_image(image,file_path):
+    image=image.asnumpy()
+    image=np.swapaxes(image,1,2)
+    image=np.swapaxes(image,2,3)
+    image=image.squeeze()
+    plt.imshow(image)
+    plt.savefig(file_path)
 
-print(im.shape)
 
-print(im)
-'''
+import numpy as np
+import cv2
+
+
+def crop_and_resize(file_path, size=256):
+    image = resize_image(file_path)
+    h, w = image.shape[:2]
+    y = (h-size)//2
+    x = (w-size)//2
+    if len(image.shape)==2 :
+        image=np.expand_dims(image,2).repeat(3,axis=2)
+        print("??????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????")
+        print(image.shape)
+    im = image[y:y+size, x:x+size, :]
+    im=np.swapaxes(im,1,2)
+    im=np.swapaxes(im,0,1)
+    im=np.reshape(im,(1,im.shape[0],im.shape[1],im.shape[2]))
+    return im
+
+
+def resize_image(file_path, size=256, bias=5):
+    image = np.array(Image.open(file_path),dtype='float32')
+    image_shape = image.shape
+    size_min = np.min(image_shape[:2])
+    size_max = np.max(image_shape[:2])
+    scale = 256 / float(size_min)
+    image = cv2.resize(image, dsize=(0, 0), fx=scale, fy=scale)
+    return image
+
+
+x=crop_and_resize("/data/imagenet/train/n01491361/n01491361_7487.JPEG")
+print(x.shape)
